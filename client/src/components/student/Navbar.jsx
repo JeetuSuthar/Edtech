84a5c3651397c818d038af13245618df83baab72
jeetu-crafteak <jeetusuthar2315@gmail.com>
2025-07-17 23:58:17 +0530

    Auth clerk

--- client/src/components/student/Navbar.jsx
+++ client/src/components/student/Navbar.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
 import { assets } from '../../assets/assets'
 import {Link} from 'react-router-dom'
+import { useClerk, UserButton,SignedIn, SignedOut, SignInButton ,useUser} from '@clerk/clerk-react'
 const Navbar = () => {
   const isCourseListPage=location.pathname.includes('course-list')
+  const {openSignin}=useClerk()
+  const {user}=useUser()
   return (
     <div className={`flex items-center justify-between px-4 sm:px-10 md:px-14 lg:px-36 border-b border-gray-500 py-4 ${isCourseListPage ? 'bg-white': 'bg-cyan-100/70'}`}>
       <img src={assets.logo} alt="Logo" className='w-28 lg:w-32
@@ -10,20 +13,34 @@ const Navbar = () => {
       <div className='hidden md:flex items-center 
       gap-5 text-gray-500'>
         <div>
-          <button>Become Educator</button>
+          {user &&
+           <><button>Become Educator</button>
           <Link to='/my-enrollments' >My Enrollments</Link>
+          </>
+}
         </div>
-        <button className='bg-blue-600 text-white px-5 py-2
-         rounded-full'>Create Account</button>
+      {user ? (
+  <UserButton />
+) : (
+  <SignInButton mode="modal">
+    <button className='bg-blue-600 text-white px-5 py-2 rounded-full'>
+      Create Account
+    </button>
+  </SignInButton>
+)}
       </div>
 
       {/* FOR MOBILE SCREEN */}
       <div className='md:hidden flex items-center gap-2 sm:gap-5 text-gray-500'>
-        <div>
-          <button>Become Educator</button>
+        <div className='flex items-center gap-1 sm:gap-2 max-sm:text-xs'>
+          {user &&
+           <><button>Become Educator</button>
           <Link to='/my-enrollments' >My Enrollments</Link>
+          </>
+}
         </div>
-        <button><img src={assets.user_icon} alt="UserIcon" /></button>
+        {user? <UserButton/>:<button onClick={()=>openSignin()}><img src={assets.user_icon} alt="UserIcon" /></button>}
+        
       </div>
     </div>
   )
